c054842b3bbccaf0df83d19e816b7ddbeda252a6
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-09-05 09:52:05 +0100

    Improvement(header and lend) added title and div for interacting with dapp

---
+++ src/components/Lend.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Lend.css";
+
+export default class Lend extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <p>Lend</p>
+        <input></input>
+      </div>
+    );
+  }
+}
